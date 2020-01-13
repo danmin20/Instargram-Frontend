38f1e53a59121abb6bfb7f20cc8e8e0080d97cc5
Lee Jeong Min <danmin20@khu.ac.kr>
2020-01-13 16:01:12 +0900

    components

---
+++ src/Components/Header.js
@@ -0,0 +1,118 @@
+import React from "react";
+import styled from "styled-components";
+import { Link, withRouter } from "react-router-dom";
+import { gql } from "apollo-boost";
+import Input from "./Input";
+import useInput from "../Hooks/useInput";
+import { Compass, HeartEmpty, User, Logo } from "./Icons";
+import { useQuery } from "react-apollo-hooks";
+
+const Header = styled.header`
+  width: 100%;
+  border: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: white;
+  border-bottom: ${props => props.theme.boxBorder};
+  border-radius: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 23px 0px;
+  z-index: 2;
+`;
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  max-width: ${props => props.theme.maxWidth};
+  display: flex;
+  justify-content: center;
+`;
+
+const HeaderColumn = styled.div`
+  text-align: center;
+  width: 33%;
+  &:first-child {
+    margin-left: 20px;
+    margin-right: auto;
+    justify-content: left;
+    text-align: left;
+  }
+  &:last-child {
+    margin-left: auto;
+    margin-right: 25px;
+    justify-content: right;
+    text-align: right;
+  }
+`;
+
+const SearchInput = styled(Input)`
+  background-color: ${props => props.theme.bgColor};
+  padding: 5px;
+  font-size: 14px;
+  border-radius: 3px;
+  height: auto;
+  text-align: center;
+  width: 220px;
+  &::placeholder {
+    opacity: 0.8;
+    font-weight: 200;
+  }
+`;
+
+const HeaderLink = styled(Link)`
+  &:not(:last-child) {
+    margin-right: 30px;
+  }
+`;
+
+const ME = gql`
+  {
+    me {
+      username
+    }
+  }
+`;
+
+export default withRouter(({ history }) => {
+  const search = useInput("");
+  const { data } = useQuery(ME);
+  const onSearchSubmit = e => {
+    e.preventDefault();
+    history.push(`/search?term=${search.value}`);
+  };
+  return (
+    <Header>
+      <HeaderWrapper>
+        <HeaderColumn>
+          <Link to="/">
+            <Logo />
+          </Link>
+        </HeaderColumn>
+        <HeaderColumn>
+          <form onSubmit={onSearchSubmit}>
+            <SearchInput {...search} placeholder="Search" />
+          </form>
+        </HeaderColumn>
+        <HeaderColumn>
+          <HeaderLink to="/explore">
+            <Compass />
+          </HeaderLink>
+          <HeaderLink to="/notifications">
+            <HeartEmpty />
+          </HeaderLink>
+          {!(data && data.me) ? (
+            <HeaderLink to="/#">
+              <User />
+            </HeaderLink>
+          ) : (
+            <HeaderLink to={data.me.username}>
+              <User />
+            </HeaderLink>
+          )}
+        </HeaderColumn>
+      </HeaderWrapper>
+    </Header>
+  );
+});
